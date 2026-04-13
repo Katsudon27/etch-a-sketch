3db17a62ccd80d2f57a6953c9627c93f3183bdc2
Katsudon27 <katsudon1127@gmail.com>
2026-04-13 21:17:40 +1000

    Add generate new grid based on user input + clearing of drawing

--- javascript.js
+++ javascript.js
@@ -1,17 +1,50 @@
-const gridContainer = document.getElementById("gridContainer");
+function createNewGrid(){
+    let num = prompt("Please enter the number of squares per side for the new grid (1 - 100).");
 
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
-        const square = document.createElement("div");
-        square.classList.add("gridSquare");
-        gridContainer.appendChild(square);
+    if (num > 100){
+        createNewGrid();
+    }
+
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    createGridSquares(Number(num));
+
+    squareDimension = 672 / Number(num);
+
+    const squares = document.querySelectorAll("div.gridSquare");
+
+    squares.forEach(element => {
+        element.style.width = squareDimension - 2 + "px";
+        element.style.height = squareDimension - 2 + "px";
+    })
+}
+
+function createGridSquares(num){
+    for (let i = 0; i < num; i++){
+        for (let j = 0; j < num; j++){
+            const square = document.createElement("div");
+            square.classList.add("gridSquare");
+            square.addEventListener('mouseenter', function(){
+                square.style.backgroundColor = "blue";
+            })
+            gridContainer.appendChild(square);
+        }
     }
 }
 
-const squares = document.querySelectorAll("div.gridSquare");
+const gridContainer = document.getElementById("gridContainer");
+createGridSquares(16);
 
-squares.forEach(element => {
-    element.addEventListener('mouseover', function(){
-        element.style.backgroundColor = "blue";
+const createGridBtn = document.getElementById("createBtn");
+createGridBtn.addEventListener("click", createNewGrid);
+
+const clearGridBtn = document.getElementById("clearBtn");
+clearGridBtn.addEventListener("click", () => {
+    const squares = document.querySelectorAll("div.gridSquare");
+
+    squares.forEach(element => {
+        element.style.backgroundColor = "initial";
     })
 });
