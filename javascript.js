e16f6a5a2655d333291c7e14ef91e00d59753da2
Katsudon27 <katsudon1127@gmail.com>
2026-04-13 20:20:26 +1000

    Add hover effect for grid squares

--- javascript.js
+++ javascript.js
@@ -2,8 +2,16 @@ const gridContainer = document.getElementById("gridContainer");
 
 for (let i = 0; i < 16; i++){
     for (let j = 0; j < 16; j++){
-        square = document.createElement("div");
+        const square = document.createElement("div");
         square.classList.add("gridSquare");
         gridContainer.appendChild(square);
     }
 }
+
+const squares = document.querySelectorAll("div.gridSquare");
+
+squares.forEach(element => {
+    element.addEventListener('mouseover', function(){
+        element.style.backgroundColor = "blue";
+    })
+});
